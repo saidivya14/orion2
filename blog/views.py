9bc37176cf0e47ed4606df52b84dcdd7514dc5af
saidivya14 <44163422+saidivya14@users.noreply.github.com>
2020-07-22 14:35:37 +0530

    made changes

--- blog/views.py
+++ blog/views.py
@@ -25,37 +25,13 @@ stripe.api_key = "sk_test_51H0rNqD5fba9rsNuuXZqKUJlFtWrvxVLV7PkZZKMM8nwB7AXnEHHi
 
 def home(request):
 	return render(request,'blog/home.html')
+
 def about(request):
 	return render(request,'blog/about.html')
+
 def prohome(request):
 	return render(request,'blog/home.html')
-def stripecheck(request,pk):
-	product = Post.objects.get(id=pk)
-	context = {'product':product}
-	return render(request,'blog/stripecheck.html',context)
-def charge(request,pk):
-	product = Post.objects.get(id=pk)
-	amount=product.final_value
-	if request.method == 'POST':
-		print('Data:', request.POST)
-		customer = stripe.Customer.create(
-			email=request.POST['email'],
-			name=request.POST['name'],
-			source=request.POST['stripeToken']
-			)
-		charge = stripe.Charge.create(
-			customer=customer,
-			amount=amount*100,
-			currency='inr',
-			description="Orion payment"
-			)
-	product.ispaid()
-	Order.objects.create(product=product,username=product.winner)
-	return redirect(reverse('success'))
 
-def successMsg(request):
-	return render(request, 'blog/success.html')
-	
 def register(request):
 	if request.method== 'POST':
 		form=UserRegisterForm(request.POST)
@@ -123,6 +99,34 @@ def contact(request):
 		'form': form_class,
 	})
 
+def stripecheck(request,pk):
+	product = Post.objects.get(id=pk)
+	context = {'product':product}
+	return render(request,'blog/stripecheck.html',context)
+
+def charge(request,pk):
+	product = Post.objects.get(id=pk)
+	amount=product.final_value
+	if request.method == 'POST':
+		print('Data:', request.POST)
+		customer = stripe.Customer.create(
+			email=request.POST['email'],
+			name=request.POST['name'],
+			source=request.POST['stripeToken']
+			)
+		charge = stripe.Charge.create(
+			customer=customer,
+			amount=amount*100,
+			currency='inr',
+			description="Orion payment"
+			)
+	product.ispaid()
+	Order.objects.create(product=product,username=product.winner)
+	return redirect(reverse('success'))
+
+def successMsg(request):
+	return render(request, 'blog/success.html')
+	
 def address(request,pk):
 	product = Post.objects.get(id=pk)
 	form_class = AddressForm
@@ -161,6 +165,7 @@ def address(request,pk):
 	return render(request, 'blog/checkout.html', {
 		'form': form_class,
 	})
+
 def activate(request, uidb64, token):
 	try:
 		uid = force_text(urlsafe_base64_decode(uidb64))
@@ -176,6 +181,7 @@ def activate(request, uidb64, token):
 	else:
 		return HttpResponse('Activation link is invalid!')	
 
+@login_required
 def my_wishlist(request):
 	posts = Wishlist.objects.filter(username=request.user)
 	for a in posts:
@@ -188,9 +194,23 @@ def my_wishlist(request):
 	}
 	return render(request,'blog/my_wishlist.html',context)
 
+@login_required
 def add_to_wishlist(request,pid):
 	product = Post.objects.get(pk=pid)
-	Wishlist.objects.create(product=product,username=request.user)
+	ws = Wishlist.objects.filter(product=product).filter(username=request.user)
+	posts1 = Wishlist.objects.filter(username=request.user)
+	for a in posts1:
+		a.product.resolve()
+	paginator = Paginator(posts1,6)
+	page = request.GET.get('page')
+	posts1 = paginator.get_page(page)
+	context1={
+		'items' : posts1
+	}
+	if ws:
+		return render(request,'blog/my_wishlist.html',context1)
+	else:
+		Wishlist.objects.create(product=product,username=request.user)
 	posts = Wishlist.objects.filter(username=request.user)
 	for a in posts:
 		a.product.resolve()
@@ -202,6 +222,7 @@ def add_to_wishlist(request,pid):
 	}
 	return render(request,'blog/my_wishlist.html',context)
 
+@login_required
 def delete_from_wishlist(request,pid):
 	product = Wishlist.objects.get(pk=pid)
 	product.delete()
@@ -228,6 +249,7 @@ def getmyitems(request):
 		'items' : posts
 	}
 	return render(request,'blog/myitems.html',context)
+
 @login_required
 def getmyorders(request):
 	posts = Order.objects.filter(username=request.user)
@@ -268,6 +290,7 @@ def auctions(request):
 		'items' : latest_auction_list
 	}
 	return render(request,'blog/auctions.html',context)
+
 @login_required 	
 def shop(request):
 	auction_list = Post.objects.all()
@@ -280,7 +303,175 @@ def shop(request):
 	context={
 		'items' : latest_auction_list
 	}
-	return render(request,'blog/shop.html',context)	
+	return render(request,'blog/shop.html',context)		
+
+@login_required
+def upauctions(request):
+	auction_list = Post.objects.all()
+	for a in auction_list:
+		a.resolve()
+	latest_auction_list = auction_list.filter(is_active=False).filter(is_exp=False).order_by('-date_added')
+	paginator = Paginator(latest_auction_list,6)
+	page = request.GET.get('page')
+	latest_auction_list = paginator.get_page(page)
+	context={
+		'items' : latest_auction_list
+	}
+	return render(request,'blog/upcoming_auctions.html',context)
+
+@login_required
+def search_upcoming(request):
+	query=request.GET.get('q3')
+	if query:
+		posts = Post.objects.all().filter(is_active=False).filter(is_exp=False)
+		results=posts.filter(Q(title__icontains=query)|Q(category__icontains=query)).order_by('-date_added')
+	else:
+		results=Post.objects.all().filter(is_active=False).filter(is_exp=False).order_by('-date_added')
+	if not results:
+		return render(request,'blog/nosearch_upcoming.html')
+	for a in results:
+		a.resolve()
+	paginator = Paginator(results,6)
+	page = request.GET.get('page')
+	results = paginator.get_page(page)
+	context={
+		'items' : results
+	}
+	return render(request,'blog/upcoming_auctions.html',context)
+
+@login_required
+def search_auctions(request):
+	query=request.GET.get('q1')
+	if query:
+		posts = Post.objects.all().filter(is_exp=True).filter(is_verified=True)
+		results=posts.filter(Q(title__icontains=query)|Q(category__icontains=query)).order_by('-date_added')
+	else:
+		results=Post.objects.all().filter(is_exp=True).order_by('-date_added')
+	if not results:
+		return render(request,'blog/nosearch_auctions.html')
+	for a in results:
+		a.resolve()
+	paginator = Paginator(results,6)
+	page = request.GET.get('page')
+	results = paginator.get_page(page)
+	context={
+		'items' : results
+	}
+	return render(request,'blog/auctions.html',context)
+
+@login_required
+def search_shop(request):
+	query=request.GET.get('q2')
+	if query:
+		posts = Post.objects.all().filter(is_active=True).filter(is_verified=True)
+		results=posts.filter(Q(title__icontains=query)|Q(category__icontains=query)).order_by('-date_added')
+	else:
+		results=Post.objects.all().filter(is_active=True).order_by('-date_added')
+	if not results:
+		return render(request,'blog/nosearch_shop.html')
+	for a in results:
+		a.resolve()
+	paginator = Paginator(results,6)
+	page = request.GET.get('page')
+	results = paginator.get_page(page)
+	context={
+		'items' : results
+	}
+	return render(request,'blog/shop.html',context)
+
+@login_required
+def shop_item(request,pid):
+	products = Post.objects.get(pk=pid)
+	products.resolve()
+	posts = Bid.objects.filter(bidder=request.user).filter(auction=products).first()
+	already_bid = False
+	if request.user.is_authenticated:
+		if products.author == request.user:
+			own_auction = True
+			return render(request, 'blog/shop1.html', {'products': products, 'own_auction': own_auction,'bid': posts,})
+
+		user_bid = Bid.objects.filter(bidder=request.user).filter(auction=products).first()
+		if user_bid:
+			already_bid = True
+			bid_amount = user_bid.amount
+			return render(request, 'blog/shop1.html', {'products': products, 'already_bid': already_bid, 'bid_amount': bid_amount,'bid': posts,})
+	return render(request,'blog/shop1.html',{'products' : products, 'already_bid': already_bid,'bid': posts,})
+
+@login_required
+def profile(request):
+	if request.method== 'POST':
+		u_form=UserUpdateForm(request.POST,instance=request.user)
+		p_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			messages.success(request,f'Your account is updated ')
+			return redirect('profile')
+	else:
+		u_form=UserUpdateForm(instance=request.user)
+		p_form=ProfileUpdateForm(instance=request.user.profile)
+	context={
+		'u_form' :u_form,
+		'p_form' :p_form
+		
+	}
+	return render(request,'blog/profile.html',context)
+
+@login_required
+def get_name(request):
+	if request.method == 'POST':	
+		form = SellForm(request.POST,request.FILES,author=request.user)	
+		if form.is_valid():
+			print("in get_name")
+			form.save()
+			return redirect('blog-home')
+	else :
+		form=SellForm(author=request.user)
+	
+	return render(request, 'blog/sell.html', {'form': form})
+ 
+# Bid on some auction
+@login_required
+def bid(request, auction_id):
+	auction = Post.objects.get(pk=auction_id)
+	auction.resolve()
+	bid = Bid.objects.filter(bidder=request.user).filter(auction=auction).first()
+
+	if not auction.is_active:
+		return render(request, 'blog/shop1.html', {
+			'auction': auction,
+		})
+
+	try:
+		bid_amount = request.POST['amount']
+		# Prevent user from entering an empty or invalid bid
+		if not bid_amount or int(bid_amount) < auction.minprice:
+			raise(KeyError)
+		if not bid:
+			# Create new Bid object if it does not exist
+			bid = Bid()
+			bid.auction = auction
+			bid.bidder = request.user
+		bid.amount = bid_amount
+		bid.date = datetime.now(timezone.utc)
+	except (KeyError):
+		# Redisplay the auction details.
+		return render(request, 'blog/shop1.html', {
+			'auction': auction,
+		})
+	else:
+		bid.save()
+		my_bids_list = Bid.objects.filter(bidder=request.user).order_by('-date')
+		for a in my_bids_list:
+			a.auction.resolve()
+		paginator = Paginator(my_bids_list,6)
+		page = request.GET.get('page')
+		my_bids_list = paginator.get_page(page)
+		context = {
+			'my_bids_list': my_bids_list,
+		}
+		return render(request,'blog/my_bids.html',context)
+
 
 def pricerange1(request):
 	auction_list = Post.objects.filter(is_active=True).filter(minprice__range=(0,10000))
@@ -388,165 +579,4 @@ def pricerange9(request):
 	context={
 		'items' : auction_list
 	}
-	return render(request,'blog/upcoming_auctions.html',context)	
-
-@login_required
-def upauctions(request):
-	auction_list = Post.objects.all()
-	for a in auction_list:
-		a.resolve()
-	latest_auction_list = auction_list.filter(is_active=False).filter(is_exp=False).order_by('-date_added')
-	paginator = Paginator(latest_auction_list,6)
-	page = request.GET.get('page')
-	latest_auction_list = paginator.get_page(page)
-	context={
-		'items' : latest_auction_list
-	}
 	return render(request,'blog/upcoming_auctions.html',context)
-
-def search_upcoming(request):
-	query=request.GET.get('q3')
-	if query:
-		posts = Post.objects.all().filter(is_active=False).filter(is_exp=False)
-		results=posts.filter(Q(title__icontains=query)|Q(category__icontains=query)).order_by('-date_added')
-	else:
-		results=Post.objects.all().filter(is_active=False).filter(is_exp=False).order_by('-date_added')
-	if not results:
-		return render(request,'blog/nosearch_upcoming.html')
-	for a in results:
-		a.resolve()
-	paginator = Paginator(results,6)
-	page = request.GET.get('page')
-	results = paginator.get_page(page)
-	context={
-		'items' : results
-	}
-	return render(request,'blog/upcoming_auctions.html',context)
-def search_auctions(request):
-	query=request.GET.get('q1')
-	if query:
-		posts = Post.objects.all().filter(is_exp=True)
-		results=posts.filter(Q(title__icontains=query)|Q(category__icontains=query)).order_by('-date_added')
-	else:
-		results=Post.objects.all().filter(is_exp=True).order_by('-date_added')
-	if not results:
-		return render(request,'blog/nosearch_auctions.html')
-	for a in results:
-		a.resolve()
-	paginator = Paginator(results,6)
-	page = request.GET.get('page')
-	results = paginator.get_page(page)
-	context={
-		'items' : results
-	}
-	return render(request,'blog/auctions.html',context)
-def search_shop(request):
-	query=request.GET.get('q2')
-	if query:
-		posts = Post.objects.all().filter(is_active=True)
-		results=posts.filter(Q(title__icontains=query)|Q(category__icontains=query)).order_by('-date_added')
-	else:
-		results=Post.objects.all().filter(is_active=True).order_by('-date_added')
-	if not results:
-		return render(request,'blog/nosearch_shop.html')
-	for a in results:
-		a.resolve()
-	paginator = Paginator(results,6)
-	page = request.GET.get('page')
-	results = paginator.get_page(page)
-	context={
-		'items' : results
-	}
-	return render(request,'blog/shop.html',context)
-
-@login_required
-def shop_item(request,pid):
-	products = Post.objects.get(pk=pid)
-	products.resolve()
-	posts = Bid.objects.filter(bidder=request.user).filter(auction=products).first()
-	already_bid = False
-	if request.user.is_authenticated:
-		if products.author == request.user:
-			own_auction = True
-			return render(request, 'blog/shop1.html', {'products': products, 'own_auction': own_auction,'bid': posts,})
-
-		user_bid = Bid.objects.filter(bidder=request.user).filter(auction=products).first()
-		if user_bid:
-			already_bid = True
-			bid_amount = user_bid.amount
-			return render(request, 'blog/shop1.html', {'products': products, 'already_bid': already_bid, 'bid_amount': bid_amount,'bid': posts,})
-	return render(request,'blog/shop1.html',{'products' : products, 'already_bid': already_bid,'bid': posts,})
-@login_required
-def profile(request):
-	if request.method== 'POST':
-		u_form=UserUpdateForm(request.POST,instance=request.user)
-		p_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
-		if u_form.is_valid() and p_form.is_valid():
-			u_form.save()
-			p_form.save()
-			messages.success(request,f'Your account is updated ')
-			return redirect('profile')
-	else:
-		u_form=UserUpdateForm(instance=request.user)
-		p_form=ProfileUpdateForm(instance=request.user.profile)
-	context={
-		'u_form' :u_form,
-		'p_form' :p_form
-		
-	}
-	return render(request,'blog/profile.html',context)
-
-@login_required
-def get_name(request):
-	if request.method == 'POST':	
-		form = SellForm(request.POST,request.FILES,author=request.user)	
-		if form.is_valid():
-			print("in get_name")
-			form.save()
-			return redirect('blog-home')
-	else :
-		form=SellForm(author=request.user)
-	
-	return render(request, 'blog/sell.html', {'form': form})
- 
-# Bid on some auction
-@login_required
-def bid(request, auction_id):
-	auction = Post.objects.get(pk=auction_id)
-	auction.resolve()
-	bid = Bid.objects.filter(bidder=request.user).filter(auction=auction).first()
-
-	if not auction.is_active:
-		return render(request, 'blog/shop1.html', {
-			'auction': auction,
-		})
-
-	try:
-		bid_amount = request.POST['amount']
-		# Prevent user from entering an empty or invalid bid
-		if not bid_amount or int(bid_amount) < auction.minprice:
-			raise(KeyError)
-		if not bid:
-			# Create new Bid object if it does not exist
-			bid = Bid()
-			bid.auction = auction
-			bid.bidder = request.user
-		bid.amount = bid_amount
-		bid.date = datetime.now(timezone.utc)
-	except (KeyError):
-		# Redisplay the auction details.
-		return render(request, 'blog/shop1.html', {
-			'auction': auction,
-		})
-	else:
-		bid.save()
-		my_bids_list = Bid.objects.filter(bidder=request.user).order_by('-date')
-		for a in my_bids_list:
-			a.auction.resolve()
-		paginator = Paginator(my_bids_list,6)
-		page = request.GET.get('page')
-		my_bids_list = paginator.get_page(page)
-		context = {
-			'my_bids_list': my_bids_list,
-		}
-		return render(request,'blog/my_bids.html',context)
